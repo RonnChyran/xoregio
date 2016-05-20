6641e5e0f620eaf63c1aa3c7e35b6439155647ae
RonnChyran <ronny6993@gmail.com>
2016-05-20 09:53:26 -0400

    Implement markBoard

---
+++ com.xoregio/XOregioMenu.java
@@ -0,0 +1,13 @@
+package com.xoregio;
+
+import javax.swing.*;
+
+/**
+ * Created by Ronny on 2016-05-20.
+ */
+public class XOregioMenu
+{
+    private JButton singlePlayerButton;
+    private JPanel panel1;
+    private JButton multiplayerButton;
+}
