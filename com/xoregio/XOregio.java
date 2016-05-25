39e9e183e3053e7ed0a56f645f33f28d5afbcd94
Ronny Chan <ronny6993@gmail.com>
2016-05-24 21:19:01 -0400

    add logo and ui

--- com/xoregio/XOregio.java
+++ com/xoregio/XOregio.java
@@ -13,38 +13,58 @@ public class XOregio
     public XOregio()
     {
         JFrame frame = new JFrame("XOregio");
+        JPanel menu = new JPanel();
+        menu.setLayout(new GridLayout(1, 2));
+        JLabel logo = new JLabel(new ImageIcon("logo.png"));
+
+        JButton spButton = new JButton("Start Single Player");
+        JButton mpButton = new JButton("Start Multi Player");
+        logo.setPreferredSize(new Dimension(400, 240));
+
+        frame.setLayout(new GridLayout(2,1));
+        frame.add(logo);
+
+        menu.add(spButton);
+        menu.add(mpButton);
+
+        frame.add(menu);
+
+
+
+        //
+        //
+        //
+      /*  JButton b = new JButton("Just fake button");
+        Dimension buttonSize = b.getPreferredSize();
+        frame.setPreferredSize(new Dimension(400,480));
+        frame.add(new JButton("Button 1"));
+        frame.add(new JButton("Button 2"));
+        frame.add(new JButton("Button 3"));
+        frame.add(new JButton("Long-Named Button 4"));
+        frame.add(new JButton("5"));*/
+        frame.setSize(400, 480);
+        frame.setVisible(true);
+      /*  JPanel gamePanel = new JPanel(new BorderLayout());
         XOregioBoard board = new XOregioBoard();
+        JLabel message = new JLabel();
+        message.setSize(400, 30);
+        message.setText("Hello World");
         JButton spButton = new JButton("Single Player");
         spButton.setFont(Font.getFont("Courier New"));
-        frame.setSize(400, 480);
         board.setSize(400, 480);
-        frame.add(board, BorderLayout.CENTER);
+        board.setMaximumSize(new Dimension(400, 480));
+        gamePanel.add(board, BorderLayout.CENTER);
+        gamePanel.add(message, BorderLayout.CENTER);
+        gamePanel.setSize(400, 480);
+        frame.setMinimumSize(new Dimension(gamePanel.getWidth(), gamePanel.getHeight() + 50));
+        frame.add(gamePanel, BorderLayout.CENTER);
         frame.add(spButton, BorderLayout.SOUTH);
+        frame.add(message, BorderLayout.SOUTH);
+        gamePanel.setVisible(false);
+        frame.pack();
         frame.setVisible(true);
 
-        //   JButton spButton = new JButton("Single Player");
-    //    JButton mpButton = new JButton("Multi Player");
-
-     /*   frame.add(spButton, BorderLayout.LINE_START);
-        frame.add(mpButton, BorderLayout.LINE_END);
-        frame.setSize(400, 480);
-        frame.setVisible(true);
-        spButton.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                new XOregioGame(new XOregioHumanPlayer(), new XOregioCPUPlayer());
-            }
-        });
-        mpButton.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                new XOregioGame(); //defaults to 2 human players
-            }
-        });*/
+*/
     }
     public static void main(String[] args)
     {
