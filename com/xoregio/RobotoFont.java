f312b31136f03a59c38a7335a626c9831e6f2956
Ronny Chan <ronny6993@gmail.com>
2016-05-27 23:00:21 -0400

    No idea why this works

--- com/xoregio/RobotoFont.java
+++ com/xoregio/RobotoFont.java
@@ -7,28 +7,18 @@ import java.io.File;
  * Represents the 'Roboto' font
  */
 //todo put this in main but im too done to do this at the moment.
-public class RobotoFont
-{
-    /**
-     * The static instance of the Roboto font
-     */
-    public static Font ROBOTO_FONT = RobotoFont.getFont();
+public class RobotoFont {
+    public static Font ROBOTO_FONT;
 
-    private static Font getFont()
-    {
-        try
-        {
-            Font font = Font.createFont(Font.TRUETYPE_FONT, new File("roboto.ttf"));
+    static {
+        try {
+
+            RobotoFont.ROBOTO_FONT = Font.createFont(Font.TRUETYPE_FONT, new File("roboto.ttf"));
             GraphicsEnvironment ge =
                     GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(RobotoFont.ROBOTO_FONT);
-            return font;
-        }
-        catch (Exception e)
-        {
-            return null;
+        } catch (Exception e) {
+
         }
     }
-
-
 }
